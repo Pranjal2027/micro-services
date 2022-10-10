863808d780c35ff4cdb6a59048d6f3f6c382c28e
Pranjal2027 <110523170+Pranjal2027@users.noreply.github.com>
2022-10-10 16:22:03 +0530

    Add files via upload

---
+++ Pay_Now_Part_Payment.java
@@ -0,0 +1,40 @@
+package invoice_testing;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import com.Xuriti.New.elevenApril.HomePage;
+import com.Xuriti.New.elevenApril.Invoices_Pay_Now_Payable_Amount;
+import com.Xuriti.New.elevenApril.Main_LoginPage;
+
+public class Pay_Now_Part_Payment {
+	WebDriver driver;
+	@Test(priority=-1)
+public void LaunchXuriti() {
+		
+		HomePage hm = new HomePage(driver);
+		driver=hm.lauchBrowser("chrome");
+		driver.get(HomePage.WEB_CONTEXT + "auth/login");
+		hm.lauchApp();
+		System.out.println("App launched successfully");
+	}
+	@Test(priority=0)
+	public void Main_Login() throws InterruptedException {
+//		driver = new ChromeDriver();
+//		driver.get(HomePage.WEB_CONTEXT + "auth/register");
+		//driver.get("https://dev.xuriti.app/#/auth/login");
+		//driver.get("https://192.168.0.153/#/auth/register");
+		driver.manage().window().maximize();
+		Main_LoginPage li=new Main_LoginPage(driver);
+		li.EmailPass("varsha.patil@tech-trail.com","Xuriti#10");	
+		//		li.EmailPass("kashif.iqbal@tech-trail.com","Kashif123@");		
+
+}
+	@Test(priority=1)
+	public void Pay_Now_Part_Payment() throws InterruptedException {
+	Invoices_Pay_Now_Payable_Amount IB= new Invoices_Pay_Now_Payable_Amount(driver);
+	IB.Pay_Now_Part_Amount("10000");
+	//IB.Pay_Now_Part_Amount();
+    driver.close();
+	
+	}
+	}
